6e8bfd00d0776c556286685908a4e313c5e0a58d
STOCHASTIC-DAVEY <42082023+STOCHASTIC-DAVEY@users.noreply.github.com>
2025-07-06 17:39:05 +1000

    push
    push

--- Math2001/02_Proofs_with_Structure/04_And.lean
+++ Math2001/02_Proofs_with_Structure/04_And.lean
@@ -13,14 +13,18 @@ example {x y : ℤ} (h : 2 * x - y = 4 ∧ y - x + 1 = 2) : x = 5 := by
     _ = 5 := by ring
 
 
+#check abs_le_of_sq_le_sq'
+
 example {p : ℚ} (hp : p ^ 2 ≤ 8) : p ≥ -5 := by
   have hp' : -3 ≤ p ∧ p ≤ 3
   · apply abs_le_of_sq_le_sq'
     calc
       p ^ 2 ≤ 9 := by addarith [hp]
-      _ = 3 ^ 2 := by numbers
+          _ = 3 ^ 2 := by numbers
     numbers
-  sorry
+  obtain ⟨hp1, hp2⟩ := hp'
+  addarith [hp1]
+
 
 example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) : a = 9 ∧ b = 1 := by
   constructor
@@ -49,29 +53,101 @@ example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a = 0 ∧ b = 0 := by
       a ^ 2 ≤ a ^ 2 + b ^ 2 := by extra
       _ = 0 := by rw [h1]
     extra
-  sorry
+  have h3 : b ^ 2 = 0 :=  by
+    calc
+    b ^ 2 = 0 + b ^ 2 := by ring
+      _ = a ^ 2 + b ^ 2 := by rw [h2]
+      _ = 0 := by rw [h1]
+  constructor
+  cancel 2 at h2
+  cancel 2 at h3
+
+
 
 /-! # Exercises -/
 
 
 example {a b : ℚ} (H : a ≤ 1 ∧ a + b ≤ 3) : 2 * a + b ≤ 4 := by
-  sorry
+  obtain ⟨h1, h2⟩ := H
+  calc
+    2 * a + b = a + b + a := by ring
+             _≤ 3 + 1 := by rel[h1,h2]
+             _=4 := by numbers
+
 
 example {r s : ℝ} (H : r + s ≤ 1 ∧ r - s ≤ 5) : 2 * r ≤ 6 := by
-  sorry
+  obtain ⟨ h1, h2 ⟩ := H
+  calc 2 * r = r + s +( r - s ) := by ring
+  _≤ 1 + 5 := by rel[h1,h2]
+  _≤ 6 := by numbers
 
 example {m n : ℤ} (H : n ≤ 8 ∧ m + 5 ≤ n) : m ≤ 3 := by
-  sorry
+  obtain ⟨ h1, h2 ⟩ := H
+  have h3 : m + 5 ≤ 8 := by
+    calc m + 5 ≤ n  := by rel [h2]
+              _≤ 8 := by rel [h1]
+  calc
+    m = m + 5 - 5 := by ring
+     _≤ 8 -5 := by rel[h3]
+     _=3 := by numbers
+
 
 example {p : ℤ} (hp : p + 2 ≥ 9) : p ^ 2 ≥ 49 ∧ 7 ≤ p := by
-  sorry
+have h1 : 7 ≤ p := by addarith [hp]
+constructor
+· calc
+    p ^ 2 ≥ 7 ^ 2 := by rel[h1]
+    _ = 49 := by norm_num
+· apply h1
 
 example {a : ℚ} (h : a - 1 ≥ 5) : a ≥ 6 ∧ 3 * a ≥ 10 := by
-  sorry
+have h1 : a ≥ 6 := by addarith [h]
+have h2 : 3 * a ≥ 10 := by
+  calc
+    3 * a ≥ 3 * 6 := by rel [h1]
+         _≥ 10 := by numbers
+constructor
+· apply h1
+· apply h2
 
 example {x y : ℚ} (h : x + y = 5 ∧ x + 2 * y = 7) : x = 3 ∧ y = 2 := by
-  sorry
+  obtain ⟨ h1 , h2 ⟩ := h
+  have h3 : x = 3 := by
+    calc x = 2 * (x + y) - (x + 2 * y) := by ring
+        _= 2 * 5 - 7 := by rw[h1, h2]
+        _= 3 := by numbers
+  have h4 : y = 2 := by
+    calc y = (x + y) - x := by ring
+        _=  5 - 3 := by rw[h1, h3]
+        _= 2 := by numbers
+  constructor
+  · apply h3
+  · apply h4
 
 example {a b : ℝ} (h1 : a * b = a) (h2 : a * b = b) :
-    a = 0 ∧ b = 0 ∨ a = 1 ∧ b = 1 := by
-  sorry
+    (a = 0 ∧ b = 0) ∨ (a = 1 ∧ b = 1) := by
+  have h : a = b := by
+    calc
+    a = a * b := by rw [h1]
+     _= b := by rw [h2]
+  have h3 : a * ( b - 1 ) = 0 := by
+    calc
+    a * ( b - 1 ) = a * b - a := by ring
+                 _= 0 := by addarith[h1]
+  have h3 := eq_zero_or_eq_zero_of_mul_eq_zero h3
+  obtain (h31 | h32) := h3
+  left
+  constructor
+  · apply h31
+  · calc
+    b = a := by rw [h]
+     _= 0 := by rw[h31]
+  right
+  have h4 : b = 1 := by addarith[h32]
+  have h5 : a = 1 := by
+      calc
+      a = b := by rw[h]
+       _= 1 := by rw[h4]
+  constructor
+  · apply h5
+  · apply h4
--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -11,6 +11,8 @@ example {a : ℚ} (h : ∃ b : ℚ, a = b ^ 2 + 1) : a > 0 := by
     a = b ^ 2 + 1 := hb
     _ > 0 := by extra
 
+#check le_or_gt
+#check cancel
 
 example {t : ℝ} (h : ∃ a : ℝ, a * t < 0) : t ≠ 0 := by
   obtain ⟨x, hxt⟩ := h
@@ -21,7 +23,19 @@ example {t : ℝ} (h : ∃ a : ℝ, a * t < 0) : t ≠ 0 := by
     cancel -x at hxt'
     apply ne_of_gt
     apply hxt'
-  · sorry
+  · have hxt' : 0 < x * ( - t ) := by
+      calc
+        0 < ( -x ) * t := by addarith [hxt]
+         _= x * ( -t ) := by ring
+    cancel x at hxt'
+    have hxt'' :  t < 0 := by
+      calc
+        0 = t + (- t) := by ring
+         _> t + 0 := by rel[hxt']
+         _= t := by ring
+    apply ne_of_lt
+    apply hxt''
+
 
 example : ∃ n : ℤ, 12 * n = 84 := by
   use 7
@@ -34,13 +48,23 @@ example (x : ℝ) : ∃ y : ℝ, y > x := by
 
 
 example : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 11 := by
-  sorry
+  use 6, 5
+  numbers
 
 example (a : ℤ) : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 2 * a + 1 := by
-  sorry
+  use a+1, a
+  ring
 
 example {p q : ℝ} (h : p < q) : ∃ x, p < x ∧ x < q := by
-  sorry
+  use (p + q)/ 2
+  constructor
+  · calc
+      p = p / 2 + p / 2:= by ring
+       _< p / 2 + q / 2 := by rel[h]
+       _= (p + q) / 2 := by ring
+  · calc
+      (p + q) / 2 < (q + q) / 2 := by rel[h]
+                 _= q := by ring
 
 example : ∃ a b c d : ℕ,
     a ^ 3 + b ^ 3 = 1729 ∧ c ^ 3 + d ^ 3 = 1729 ∧ a ≠ c ∧ a ≠ d := by
@@ -57,20 +81,59 @@ example : ∃ a b c d : ℕ,
 
 
 example : ∃ t : ℚ, t ^ 2 = 1.69 := by
-  sorry
+  use 1.3
+  numbers
+
 example : ∃ m n : ℤ, m ^ 2 + n ^ 2 = 85 := by
-  sorry
+  use 9, 2
+  numbers
 
 example : ∃ x : ℝ, x < 0 ∧ x ^ 2 < 1 := by
-  sorry
+  use -0.5
+  constructor
+  numbers
+  numbers
+
 example : ∃ a b : ℕ, 2 ^ a = 5 * b + 1 := by
-  sorry
+  use 4, 3
+  numbers
 
 example (x : ℚ) : ∃ y : ℚ, y ^ 2 > x := by
-  sorry
+  have H := le_or_gt x 0
+  obtain h1 | h2 := H
+  · use 0.5
+    calc 0.5 ^ 2 > 0 := by numbers
+              _≥ x := by rel[h1]
+  · use x + 1
+    calc ( x + 1 ) ^ 2 = x ^ 2 + x + x + 1 := by ring
+    _≥ 0 + x + x + 1 := by extra
+    _= x + x + 1 := by ring
+    _> 0 + x + 1 := by rel[h2]
+    _= x + 1 := by ring
+    _> x := by extra
+
 
 example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
-  sorry
+  obtain ⟨a, hat⟩ := h
+  have hx : ( 1 - a ) * ( t - 1 ) > 0 := by
+    calc
+      ( 1 - a ) * ( t - 1 ) = a + t - (1 + a * t) := by ring
+                           _> a * t + 1 - (1 + a * t) := by rel[hat]
+                           _= 0 := by ring
+  have H := le_or_gt a 1
+  obtain ( h1 | h2 ):= H
+  · apply ne_of_gt
+    have ha1 : 1 - a ≥ 0 := by addarith [h1]
+    cancel 1 - a at hx
+    addarith [hx]
+  · apply ne_of_lt
+    have ha2 : a-1 > 0 := by addarith [h2]
+    have hx' : ( a - 1 ) * ( 1 - t ) > 0 := by
+      calc
+        ( a - 1 ) * ( 1 - t ) = ( 1 - a ) * ( t - 1 ) := by ring
+                           _> 0 := by rel[hx]
+    cancel a-1 at hx'
+    addarith[hx']
 
 example {m : ℤ} (h : ∃ a, 2 * a = m) : m ≠ 5 := by
   sorry
